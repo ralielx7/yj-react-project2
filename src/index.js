2cba1f209f0333ec76b5afe2c7e21cad7f7ab19b
unknown <bjune0307@gmail.com>
2023-05-22 16:39:21 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ChakraProvider } from '@chakra-ui/react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
